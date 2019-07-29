5f2eb64636a23b984b20b42b9b361e64b6e0fc78
zhangxin <997662000@qq.com>
2019-07-29 17:08:14 +0800

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+
+    protected $table = 'users';
     use Notifiable;
+    
 
     /**
      * The attributes that are mass assignable.
